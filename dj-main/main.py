19ed8873ab46bd0b6fac677a1a9ff1bcb5b7d878
Fabio-Paula <99517131+Fabio-Paula@users.noreply.github.com>
2022-06-21 11:42:08 -0300

    Update main.py

--- dj-main/main.py
+++ dj-main/main.py
@@ -5,13 +5,6 @@ import random
 import os
 TOKEN = "TOKEN"
 PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
-# add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN,
-                      webhook_url="https://<appname>.herokuapp.com/" + TOKEN)
-updater.idle()
 
 cogs = [music]
 
